3138aa978bb8c9e35fd58941fbef7dc162e98b6f
Rijal Ghodi <rijalghodi10@gmail.com>
2023-01-19 16:18:09 +0700

    add get request on page load

---
+++ src/components/http-request/GetOnPageLoad.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Get On Page Load</h1>
+    <div v-for="user in users" :key="user.id">{{ user.name }} | {{ user.email }}</div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "GetOnPageLoad",
+  created() {
+    this.getUsers();
+  },
+  data() {
+    return {
+      users: [],
+    };
+  },
+  methods: {
+    getUsers() {
+      axios
+        .get("https://jsonplaceholder.typicode.com/users")
+        .then((response) => (this.users = response.data))
+        .catch((err) => console.log(err));
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/HTTPRequest.vue
+++ src/views/HTTPRequest.vue
@@ -2,17 +2,20 @@
   <div>
     <SimpleGet />
     <ParentLifeCycle />
+    <GetOnPageLoad />
   </div>
 </template>
 
 <script>
 import SimpleGet from "@/components/http-request/SimpleGet.vue";
 import ParentLifeCycle from "@/components/component-lifecycle/ParentLifeCycle.vue";
+import GetOnPageLoad from "@/components/http-request/GetOnPageLoad.vue";
 export default {
   name: "HTTPRequest",
   components: {
     SimpleGet,
     ParentLifeCycle,
+    GetOnPageLoad,
   },
 };
 </script>
